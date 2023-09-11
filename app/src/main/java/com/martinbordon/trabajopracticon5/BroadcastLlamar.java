4b879d385dd6946dba63352bfb1ee584ff060bb8
MartineBordon26 <martinebordon26@gmail.com>
2023-09-11 17:06:05 -0300

    arreglo if isConnected

--- app/src/main/java/com/martinbordon/trabajopracticon5/BroadcastLlamar.java
+++ app/src/main/java/com/martinbordon/trabajopracticon5/BroadcastLlamar.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 
 public class BroadcastLlamar extends BroadcastReceiver {
 
@@ -12,10 +13,12 @@ public class BroadcastLlamar extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(intent.getBooleanExtra("state", false)) {
+        boolean isConnected = intent.getBooleanExtra("connected", false);
+        Log.d("salida", isConnected+"");
+       if(isConnected) {
             llamadaIntent.setData(Uri.parse("tel:" + "2664553747"));
             llamadaIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(llamadaIntent);
-        }
+       }
     }
 }
